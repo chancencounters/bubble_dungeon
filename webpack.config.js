25cc4aa08cb10f724ad99e2dda1af8fdec19c983
Zack Yu <zack.yu2@gmail.com>
2016-12-22 20:15:01 -0500

    Add snap functionality

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,20 @@ module.exports = {
   output: {
   	filename: "./lib/bundle.js"
   },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
   devtool: 'source-map',
+  resolve: {
+    extensions: ["", ".js"]
+  }
 };
